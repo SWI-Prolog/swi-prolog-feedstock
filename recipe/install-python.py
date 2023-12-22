3d2e1ff32e03d59b03854ca4f9fc290f23791137
Jan Wielemaker <jan@swi-prolog.org>
2023-12-22 09:42:49 +0100

    Remove a few debug print statements.

--- recipe/install-python.py
+++ recipe/install-python.py
@@ -10,11 +10,8 @@ conda_utils.install_component("Python_interface")
 
 # Install the Python package
 def install_janus():
-    subprocess.run(["printenv"])          
-
     wheel = glob.glob("packages/swipy/dist/*.whl")[0];
     print(f"Installing wheel file {wheel}")
-    print(f"sys.path = {sys.path}")
 
     if subprocess.run([ "python", "-m", "pip", "install",
                         "--prefix", os.environ["PREFIX"],
